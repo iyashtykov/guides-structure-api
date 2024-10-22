855c1e5c38ba2d9ba16b8e53175347b7ca8bf0cc
Igor Igor <iyashtykov@codio.com>
2024-10-22 13:18:15 +0000

    modified

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const GET_GUIDES_STRUCTURE = 'get_guides_structure'
+const GET_GUIDES_PAGE = 'get_guides_page'
 const ADD_GUIDES_PAGE = 'add_guides_page'
 const UPDATE_GUIDES_PAGE = 'update_guides_page'
 const DELETE_GUIDES_PAGE = 'delete_guides_page'
@@ -101,11 +102,22 @@ const getGuidesStructure = async () => {
 const addGuidesPage = async () => {
     const structure = await window.codioIDE.guides.structure.getStructure()
     const lastIndex = structure.children.length
-    parentId = null    
+    parentId = null   
 
     try {
         const res = await window.codioIDE.guides.structure.add(generateSettings(), parentId, lastIndex)
         console.log('result: ', res)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+const getGuidesPage = async () => {
+    const structure = await window.codioIDE.guides.structure.getStructure()
+    const latestPageId = structure.children[structure.children.length - 1].id
+    try {
+        const page = await window.codioIDE.guides.structure.get(latestPageId)
+        console.log('get page: ', page)
     } catch (e) {
         console.error(e)
     }
@@ -139,12 +151,65 @@ const addMenuItem = (parentDescriptor, item) => {
   window.codioIDE.menu.addItem(parentDescriptor, item)
 }
 
+const style = `
+    width: 140px;
+    height: 110px;
+    z-index: 10000;
+    position: absolute;
+    left: 30px;
+    top: 600px;
+    background-color: white;
+    padding: 10px;
+`
+
 const init = () => {
+    // Use actions from menu Codio/...
     addMenuItem({title: 'Codio'}, {id: GET_GUIDES_STRUCTURE, title: 'get guides structure', callback: getGuidesStructure})
+    addMenuItem({title: 'Codio'}, {id: GET_GUIDES_PAGE, title: 'get guides page', callback: getGuidesPage})
     addMenuItem({title: 'Codio'}, {id: ADD_GUIDES_PAGE, title: 'add guides page', callback: addGuidesPage})
     addMenuItem({title: 'Codio'}, {id: UPDATE_GUIDES_PAGE, title: 'update guides page', callback: updateGuidesPage})
     addMenuItem({title: 'Codio'}, {id: DELETE_GUIDES_PAGE, title: 'delete guides page', callback: deleteGuidesPage})
 
+    // Or use actions from custom area 
+    const modal = document.createElement('div')
+    modal.id = 'customModalId'
+    modal.style = style
+    const buttonGetStructure = document.createElement('button')
+    const buttonGet = document.createElement('button')
+    const buttonAdd = document.createElement('button')
+    const buttonUpdate = document.createElement('button')
+    const buttonDelete = document.createElement('button')
+    buttonGetStructure.type = 'button'
+    buttonGet.type = 'button'
+    buttonAdd.type = 'button'
+    buttonUpdate.type = 'button'
+    buttonDelete.type = 'button'
+    buttonGet.innerHTML = 'get guides page'
+    buttonGetStructure.innerHTML = 'get guides structure'
+    buttonAdd.innerHTML = 'add guides page'
+    buttonUpdate.innerHTML = 'update guides page'
+    buttonDelete.innerHTML = 'delete guides page'
+    buttonGetStructure.onclick = () => {
+        setTimeout(getGuidesStructure,5000)
+    }
+    buttonGet.onclick = () => {
+        setTimeout(getGuidesPage,5000)
+    }
+    buttonAdd.onclick = () => {
+        setTimeout(addGuidesPage,5000)
+    }
+    buttonUpdate.onclick = () => {
+        setTimeout(updateGuidesPage,5000)
+    }
+    buttonDelete.onclick = () => {
+        setTimeout(deleteGuidesPage,5000)
+    }
+    modal.append(buttonGetStructure)
+    modal.append(buttonGet)
+    modal.append(buttonAdd)
+    modal.append(buttonUpdate)
+    modal.append(buttonDelete)
+    document.querySelector('body').append(modal)
 }
 
-setTimeout(init, 3000)
+setTimeout(init, 5000)
